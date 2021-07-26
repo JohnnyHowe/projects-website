4cdabd3197acad82b21398a9ae0f586079aabae9
Johnny Howe <jonathon.o.howe@gmail.com>
2021-07-26 20:23:03 +1200

    Google play badge and real Jump info

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
-    <div v-for="i in 1" :key="i">
     <ProjectCard
-      :img="require('@/assets/' + 'tower-dropper/' + 'screenshot.webp' + '')"
-      :video="require('@/assets/' + 'tower-dropper/' + 'gameplay.mp4' + '')"
+      :img="require('@/assets/tower-dropper/screenshot.webp')"
+      :video="require('@/assets/tower-dropper/gameplay.mp4')"
       ><h1>Tower Dropper</h1>
       <br />
       <p>
-        Tower dropper is a mobile stacking game. The higher you build the tower, the more points you get.<br/>
-        Watch out though, some dumbass keeps loading up the crane with TNT. Who thought it would be a good idea to store TNT with the buildings?
+        Tower dropper is a mobile stacking game. The higher you build the tower,
+        the more points you get.<br />
+        Watch out though, some dumbass keeps loading up the crane with TNT. Who
+        thought it would be a good idea to store TNT with the buildings?
       </p>
       <br />
-      <a target="_blank" href="https://play.google.com/store/apps/details?id=com.DefaultCompany.TowerDropper">Tower Dropper on Google Play Store</a>
+      <a
+        href="https://play.google.com/store/apps/details?id=com.DefaultCompany.TowerDropper&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+        target="_blank"
+        ><img
+          class="google-play"
+          alt="Get it on Google Play"
+          src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+      /></a>
     </ProjectCard>
-    <ProjectCard
-      :img="require('@/assets/' + 'jump/' + 'screenshot.png' + '')"
+    <ProjectCard :img="require('@/assets/jump/screenshot.png')"
       ><h1>Jump</h1>
       <br />
       <p>
-        I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda
+        Climb your way up higher and higher by launching yourself from wall to
+        wall. <br />
+        Don't take too long though, if you're too slow the acid will catch up
+        and your game will be over.
       </p>
+      <a
+        href="https://play.google.com/store/apps/details?id=com.UnluckyDuck.Jump"
+        target="_blank"
+        ><img
+          class="google-play"
+          alt="Get it on Google Play"
+          src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+      /></a>
     </ProjectCard>
-  </div>
   </div>
 </template>
 <script>
@@ -33,3 +50,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.google-play {
+  height: min(10vh, 5vw);
+}
+</style>
